f3ce2e74f06f6b086765909bee947d446a6b9207
Ivan Rozhentcev <kruzkachay@gmail.com>
2026-04-23 17:47:38 +0300

    Сделаны маршруты /health и /info

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def simple_get():
     return {"message": "Hello from FastAPI"}
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+@app.get("/info")
+def info():
+    return {"app": "devops-test",
+            "version": "1.0.0",
+            "port": 8000}
